4fe65933c7ffbeffc9e4a630ab13edd18982f4fa
Steven Kessler <stvnksslr@gmail.com>
2023-11-11 23:18:14 -0500

    feat(grafana tags): adds the service name that triggered the notification to the list of tags pushed to allow filtering on the grafana side and templating

--- internal/notifier/grafana.go
+++ internal/notifier/grafana.go
@@ -69,7 +69,8 @@ func (g *Grafana) Post(ctx context.Context, event eventv1.Event) error {
 
 	sfields := make([]string, 0, len(event.Metadata))
 	// add tag to filter on grafana
-	sfields = append(sfields, "flux", event.ReportingController)
+	sfields = append(sfields, "flux", event.ReportingController, event.InvolvedObject.Name)
+
 	for k, v := range event.Metadata {
 		sfields = append(sfields, fmt.Sprintf("%s: %s", k, v))
 	}
